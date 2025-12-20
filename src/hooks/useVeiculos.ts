5a72db124745c5eff5e44dd369eeecd5cf704cb1
Alisson <124639348+SilvAlisson@users.noreply.github.com>
2025-12-20 13:33:19 +0000

    Correção de api

--- src/hooks/useVeiculos.ts
+++ src/hooks/useVeiculos.ts
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import { api } from '../lib/api'; // Certifique-se de que o caminho está correto
+import { api } from '../services/api';
 import { toast } from 'sonner';
 
 export interface Veiculo {
