e4923bfdfa45a8c9bd037461e25b84c1c24ce429
SilvAlisson <alisson.silva0528@gmail.com>
2026-05-21 21:26:17 -0300

    fix: add self-destructing sw.js to fix MIME type errors on legacy service worker updates

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    self.registration.unregister().then(() => {
+      return self.clients.matchAll();
+    }).then((clients) => {
+      clients.forEach((client) => {
+        if (client.url && "navigate" in client) {
+          client.navigate(client.url);
+        }
+      });
+    })
+  );
+});
